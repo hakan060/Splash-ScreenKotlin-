bab072e1d327cdfd0c5c533cc01379da8d6c5603
Mehmet Hakan Alp <alphakan05@gmail.com>
2023-03-18 14:11:15 +0300

    Splash Screen
    Application waiting on the start screen for 3 seconds

---
+++ withKotlinChronometer/app/src/main/java/com/example/withkotlinchronometer/Chronometers.kt
@@ -0,0 +1,13 @@
+package com.example.withkotlinchronometer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chronometers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cronometers)
+
+
+    }
+}
